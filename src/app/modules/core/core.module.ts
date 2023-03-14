49e564a45fab9672dfbbb945fdb5babb9660829a
PawelRadawiec <pawel.radaw@gmail.com>
2023-03-14 18:44:30 +0100

    header

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
-  exports: [HeaderComponent],
+  imports: [CommonModule, NzLayoutModule, NzPageHeaderModule, NzIconModule],
+  exports: [HeaderComponent, NzLayoutModule, NzPageHeaderModule, NzIconModule],
 })
 export class CoreModule {}
