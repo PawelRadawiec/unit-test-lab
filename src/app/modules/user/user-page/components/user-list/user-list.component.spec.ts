3596da993cf561bf67b5f54470dd71be5d9bd9a9
PawelRadawiec <pawel.radaw@gmail.com>
2023-03-18 13:06:56 +0100

    user list unit tests

--- src/app/modules/user/user-page/components/user-list/user-list.component.spec.ts
+++ src/app/modules/user/user-page/components/user-list/user-list.component.spec.ts
@@ -1,16 +1,52 @@
+import { MockComponents } from 'ng-mocks';
+import { UserStateMock } from 'src/app/mocks/state/users-state-mock';
+import { NgxsModule } from '@ngxs/store';
+import { NzModalService } from 'ng-zorro-antd/modal';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { By } from '@angular/platform-browser';
+import { Component, Input, OnInit } from '@angular/core';
+import { NzDividerComponent } from 'ng-zorro-antd/divider';
+import { UserFormComponent } from '../user-form/user-form.component';
 
-xdescribe('UserListComponent', () => {
+@Component({
+  selector: 'nz-table',
+  template: `<ng-content></ng-content>`,
+})
+class NzTableMock implements OnInit {
+  @Input() nzData: any;
+  data: any;
+
+  ngOnInit() {
+    this.data = this.nzData;
+  }
+}
+
+describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let modalService: any;
 
   beforeEach(async () => {
+    modalService = jasmine.createSpyObj('NzModalService', {
+      create: undefined,
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        UserListComponent,
+        NzTableMock,
+        MockComponents(NzDividerComponent),
+      ],
+      providers: [
+        {
+          provide: NzModalService,
+          useValue: modalService,
+        },
+      ],
+      imports: [NgxsModule.forRoot([UserStateMock])],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
@@ -20,4 +56,67 @@ xdescribe('UserListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should click edit', () => {
+    const user = {
+      name: 'Jan',
+      surname: 'Nowak',
+      email: 'jan@gmail.com',
+      age: '12',
+      city: 'City',
+      id: 1,
+    };
+    spyOn(component, 'edit').and.callThrough();
+
+    const editIcon = getFirstRow().queryAll(By.css('.icon'))[0];
+    editIcon.triggerEventHandler('click');
+
+    expect(component.edit).toHaveBeenCalled();
+    expect(component['modalService'].create).toHaveBeenCalledOnceWith({
+      nzTitle: 'Edit',
+      nzContent: UserFormComponent,
+      nzComponentParams: { user },
+      nzFooter: null,
+    });
+  });
+
+  it('should click delete', () => {
+    spyOn(component, 'delete').and.callThrough();
+
+    const editIcon = getFirstRow().queryAll(By.css('.icon'))[1];
+    editIcon.triggerEventHandler('click');
+
+    expect(component.delete).toHaveBeenCalled();
+    expect(component['modalService'].create).toHaveBeenCalledWith({
+      nzTitle: 'Delete',
+      nzContent: `Delete Jan Nowak`,
+      nzOnOk: jasmine.any(Function),
+    });
+  });
+
+  it('should render table', () => {
+    const nzTable = fixture.debugElement.query(By.css('nz-table'));
+    const rows = fixture.debugElement.queryAll(By.css('tbody tr'));
+    const rowsData = rows[0].queryAll(By.css('td'));
+
+    expect(nzTable).withContext('nz-table').toBeTruthy();
+    expect(rows.length).withContext('should render body rows tr').toEqual(2);
+
+    expect(rowsData?.[0]?.nativeElement?.innerText)
+      .withContext('Name')
+      .toEqual('Jan');
+    expect(rowsData?.[1]?.nativeElement?.innerText)
+      .withContext('Surname')
+      .toEqual('Nowak');
+    expect(rowsData?.[2]?.nativeElement?.innerText)
+      .withContext('Email')
+      .toEqual('jan@gmail.com');
+    expect(rowsData?.[3]?.nativeElement?.innerText)
+      .withContext('Age')
+      .toEqual('12');
+  });
+
+  function getFirstRow() {
+    return fixture.debugElement.query(By.css('tbody tr'));
+  }
 });
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -6,7 +6,6 @@ import { UserRoutingModule } from './user-routing.module';
 import { UserPageComponent } from './user-page/user-page.component';
 import { UserFormComponent } from './user-page/components/user-form/user-form.component';
 import { UserListComponent } from './user-page/components/user-list/user-list.component';
-import { DrawerUserComponent } from './user-page/components/drawer-user/drawer-user.component';
 import { UserSearchFormComponent } from './user-page/components/user-search-form/user-search-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -15,7 +14,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     UserPageComponent,
     UserFormComponent,
     UserListComponent,
-    DrawerUserComponent,
     UserSearchFormComponent,
   ],
   imports: [
