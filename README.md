0182b15d1e2001533ec4693786a1e23483604cad
PawelRadawiec <pawel.radaw@gmail.com>
2023-05-08 20:16:00 +0200

    button unit tests

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ npm start
 
 npm test
 
+# In order to run unit tests with coverages
+
+ng test --code-coverage --no-watch or ng test --code-coverage
+
 # In order to start json-server
 
 json-server --watch db.json
--- src/app/modules/shared/components/button/button.component.spec.ts
+++ src/app/modules/shared/components/button/button.component.spec.ts
@@ -1,6 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ButtonComponent } from './button.component';
+import { ButtonComponent, ButtonType } from './button.component';
+import { Component, ViewChild } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+// @Component({
+//   template: ` <app-button
+//     [type]="type.DANGER"
+//     outline="true"
+//     [text]="'Button text'"
+//     (onClick)="onClick()"
+//   ></app-button>`,
+// })
+// class WrapperTestComponent {
+//   @ViewChild(ButtonComponent) button: ButtonComponent;
+
+//   type = ButtonType;
+
+//   onClick() {}
+// }
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
@@ -8,16 +26,35 @@ describe('ButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
-    })
-    .compileComponents();
+      declarations: [ButtonComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
+    component.type = ButtonType.DANGER;
+    component.text = 'Button text';
+    component.outline = true;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should click onClick', () => {
+    spyOn(component.onClick, 'next');
+    const buttonEl = fixture.debugElement.query(By.css('button'));
+    buttonEl.triggerEventHandler('click');
+
+    expect(component.onClick.next).toHaveBeenCalled();
+  });
+
+  it('should set button--outline and button--danger class', () => {
+    const buttonOutline = fixture.debugElement.query(By.css('.button--outline'));
+    expect(buttonOutline).withContext('button with .button--outline').toBeTruthy();
+    
+    const buttonDanger = fixture.debugElement.query(By.css('.button--danger'));
+    expect(buttonDanger).withContext('button with .button--danger').toBeTruthy();
+  })
 });
